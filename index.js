1c8b01f89ca8781a56f1672f6144e1a2b6690659
santo123457 <mdsanto123457@gmail.com>
2023-07-28 12:23:06 +0200

    server was used for full project

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hbqlggf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
